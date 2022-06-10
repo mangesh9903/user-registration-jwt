8e8753635547b033e0d06df8f53906fe7bf51b25
mangesh9903 <mangeshb9903@gmail.com>
2022-06-10 13:51:58 +0530

    ADDED: Feature of Setup for User Registartion JWT.

---
+++ src/test/java/com/bridgelabz/userregistrationjwt/UserRegistrationJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.userregistrationjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
